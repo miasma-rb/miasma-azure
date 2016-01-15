0d4282d097a4ddaa986253b2083b82a95d356c36
Chris Roberts <code@chrisroberts.org>
2016-01-15 13:38:01 -0800

    Define customized request matcher

--- test/helper.rb
+++ test/helper.rb
@@ -9,10 +9,29 @@ require 'miasma'
 require 'miasma/specs'
 
 VCR.configure do |c|
+
+  json_body_matcher = lambda do |r1, r2|
+    begin
+      r1_body = MultiJson.load(r1.body).to_smash
+      r2_body = MultiJson.load(r2.body).to_smash
+      if(r1_body[:tags])
+        r1_body[:tags].delete(:updated)
+        r1_body[:tags].delete(:created)
+        r2_body[:tags].delete(:updated)
+        r2_body[:tags].delete(:created)
+      end
+      r1_body.checksum == r2_body.checksum ||
+        (r1_body.fetch(:properties, {}).key?('templateLink') &&
+        r2_body.fetch(:properties, {}).key?('templateLink'))
+    rescue => e
+      r1.body == r2.body
+    end
+  end
+
   c.cassette_library_dir = 'test/cassettes'
   c.hook_into :webmock
   c.default_cassette_options = {
-    :match_requests_on => [:method, :body, :path]
+    :match_requests_on => [:method, :path, json_body_matcher]
   }
   c.filter_sensitive_data('MIASMA_AZURE_CLIENT_ID'){ ENV['MIASMA_AZURE_CLIENT_ID'] }
   c.filter_sensitive_data('MIASMA_AZURE_CLIENT_SECRET'){ ENV['MIASMA_AZURE_CLIENT_SECRET'] }
