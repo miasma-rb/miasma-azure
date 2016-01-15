7eae0557ab07e1e8b64df54dd2f96b2ee9ff5bfb
Chris Roberts <code@chrisroberts.org>
2016-01-14 18:42:17 -0800

    Add initial test setup for cassettes

--- test/helper.rb
+++ test/helper.rb
@@ -12,13 +12,15 @@ VCR.configure do |c|
   c.cassette_library_dir = 'test/cassettes'
   c.hook_into :webmock
   c.default_cassette_options = {
-    :match_requests_on => [:method, :body,
-      VCR.request_matchers.uri_without_params(
-
-      )
-    ]
+    :match_requests_on => [:method, :body, :path]
   }
-  c.filter_sensitive_data('AZURE_KEY'){ ENV['AZURE_KEY'] }
+  c.filter_sensitive_data('MIASMA_AZURE_CLIENT_ID'){ ENV['MIASMA_AZURE_CLIENT_ID'] }
+  c.filter_sensitive_data('MIASMA_AZURE_CLIENT_SECRET'){ ENV['MIASMA_AZURE_CLIENT_SECRET'] }
+  c.filter_sensitive_data('MIASMA_AZURE_SUBSCRIPTION_ID'){ ENV['MIASMA_AZURE_SUBSCRIPTION_ID'] }
+  c.filter_sensitive_data('MIASMA_AZURE_TENANT_ID'){ ENV['MIASMA_AZURE_TENANT_ID'] }
+  c.filter_sensitive_data('MIASMA_AZURE_BLOB_ACCOUNT_NAME'){ ENV['MIASMA_AZURE_BLOB_ACCOUNT_NAME'] }
+  c.filter_sensitive_data('MIASMA_AZURE_BLOB_SECRET_KEY'){ ENV['MIASMA_AZURE_BLOB_SECRET_KEY'] }
+  c.filter_sensitive_data('MIASMA_AZURE_REGION'){ ENV['MIASMA_AZURE_REGION'] }
 end
 
 MinitestVcr::Spec.configure!
